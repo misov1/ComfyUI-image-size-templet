4eb71a10bda93bd6a342417827e4c80034cc8136
misov_ <70870640+misov1@users.noreply.github.com>
2023-12-27 08:36:27 +0900

    Update nodes.py

--- nodes.py
+++ nodes.py
@@ -1,3 +1,5 @@
+import re
+
 class DimensionProviderRatio:
     def __init__(self):
         pass
@@ -6,21 +8,23 @@ class DimensionProviderRatio:
     def INPUT_TYPES(s):
         return {
             "required": {
-                "dimension": (["512 x 512", "512 x 768", "768 x 512", "1024 x 1024", "1152 x 896", "896 x 1152", "1216 x 832", "832 x 1216", "1344 x 768", "768 x 1344", "1536 x 640", "640 x 1536", "1280 x 720"],),
+                "dimension": (["Portrait 832x1216", "Landscape 1216x832", "Square 1024x1024", "Portrait 1024x1536", "Landscape 1536x1024", "Square 1472x1472"],),
             },
         }
 
     RETURN_TYPES = ("INT", "INT")
     RETURN_NAMES = ("ratio_width", "ratio_height")
-    FUNCTION = "prodvide_dimensions"
+    FUNCTION = "provide_dimensions"
     OUTPUT_NODE = True
     CATEGORY = "DimensionProviderRatio"
 
-    def prodvide_dimensions(self, dimension):
-        dims = ["512x512", "512x768", "768x512", "1024x1024", "1152x896", "896x1152", "1216x832", "832x1216", "1344x768", "768x1344", "1536x640", "640x1536", "1280x720"]
-        index = dims.index(dimension.replace(" ", ""))
-        img_dims = dimension.split("x")
-        return (int(img_dims[0]), int(img_dims[1]))
+    def provide_dimensions(self, dimension):
+        dims = ["832x1216", "1216x832", "1024x1024", "1024x1536", "1536x1024", "1472x1472"]
+        pattern = "[^0-9x]"
+        cleaned_dimension = re.sub(pattern, "", dimension)
+        index = dims.index(cleaned_dimension)
+        img_dims = cleaned_dimension.split("x")
+        return int(img_dims[0]), int(img_dims[1])
 
 class DimensionProviderFree:
     def __init__(self):
@@ -37,11 +41,11 @@ class DimensionProviderFree:
 
     RETURN_TYPES = ("INT", "INT")
     RETURN_NAMES = ("free_width", "free_height")
-    FUNCTION = "prodvide_dimensions"
+    FUNCTION = "provide_dimensions"
     OUTPUT_NODE = True
     CATEGORY = "DimensionProviderFree"
 
-    def prodvide_dimensions(self, width, height):
+    def provide_dimensions(self, width, height):
         return (width, height)
 
 class StringConcat:
